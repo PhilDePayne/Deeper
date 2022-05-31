bef288d0fb84c44d6209caede28d562a24dde5fd
Markus <markusderiu610@gmail.com>
2022-05-31 10:32:52 +0200

    Lights activation fix
    New light replaces only 1 light

--- lib/src/LampAI.cpp
+++ lib/src/LampAI.cpp
@@ -9,8 +9,20 @@ void LampAI::onCollisionEnter() {
 void LampAI::onTriggerEnter(BoxCollider collided) {
 
 	//printf("TRIGGERED COLLISION\n");
-	lights->erase(lights->begin());
-	glm::vec3 newLightPos = collided.getCenter();
-	lights->push_back(newLightPos);
+	bool exists = false;
+
+	for (auto i : *lights) {
+
+		if (i == collided.getCenter()) exists = true;
+
+	}
+
+	if (!exists) {
+
+		lights->erase(lights->begin());
+		//glm::vec3 newLightPos = collided.getCenter();
+		lights->push_back(collided.getCenter());
+
+	}
 
 }
