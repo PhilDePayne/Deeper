bac480ac97cd4c554652967c4727c401d0dabc97
Wojciech Baljon <229838@edu.p.lodz.pl>
2022-06-02 15:02:25 +0200

    Deleted redundant logs

--- lib/src/Animator.cpp
+++ lib/src/Animator.cpp
@@ -19,16 +19,10 @@ Animator::Animator(Animation *Animation)
 void Animator::UpdateAnimation(float dt)
 {
     DeltaTime = dt;
-    printf("In Update Animation\n");
-    printf("Delta Time: %f\n", dt);
-    printf("Current time beforeIf: %f\n", CurrentTime);
     if (CurrentAnimation)
     {
         CurrentTime += CurrentAnimation->GetTicksPerSecond() * dt;
-        printf("Current time inIF1: %f\n", CurrentTime);
         CurrentTime = fmod(CurrentTime, CurrentAnimation->GetDuration());
-        printf("Duration: %f\n", CurrentAnimation->GetDuration());
-        printf("Current time inIF2: %f\n", CurrentTime);
         CalculateBoneTransform(&CurrentAnimation->GetRootNode(), glm::mat4(1.0f));
     }
 }
@@ -41,15 +35,12 @@ void Animator::PlayAnimation(Animation *pAnimation)
 
 void Animator::CalculateBoneTransform(const AssimpNodeData *node, glm::mat4 parentTransform)
 {
-    printf("Current time CBT1: %f\n", CurrentTime);
     std::string nodeName = node->name;
     glm::mat4 nodeTransform = node->transformation;
 
     Bone* Bone = CurrentAnimation->FindBone(nodeName);
-    printf("Current time CBT2: %f\n", CurrentTime);
     if (Bone)
     {
-        printf("IN Bone IF\n");
         Bone->Update(CurrentTime);
         nodeTransform = Bone->GetLocalTransform();
     }
